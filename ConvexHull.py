6bd3e5b0e6006e5def1433dbf030eafea4890193
murtaza147 <109812861+murtaza147@users.noreply.github.com>
2022-09-27 09:18:25 -0400

    Create ConvexHull.py
    created convexHull file

---
+++ ConvexHull.py
@@ -0,0 +1,12 @@
+coordinates = [(1, 2), (4, 2), (3, 1), (4, 7), (5, 3)]
+def rightTurn(l):
+
+def convexHull(p):
+     sorted(p)
+     upper = [p[0], p[1]]
+     i = 2
+     while i < len(coordinates):
+          while len(upper) > 2 and not rightTurn
+
+
+print(convexHull(coordinates))
