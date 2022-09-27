fb953728635a447ae51e74843a52e0d8f68a3a29
murtaza147 <109812861+murtaza147@users.noreply.github.com>
2022-09-27 10:05:10 -0400

    def convexHull
    from the pseudocode in the textbook

--- ConvexHull.py
+++ ConvexHull.py
@@ -1,12 +1,24 @@
 coordinates = [(1, 2), (4, 2), (3, 1), (4, 7), (5, 3)]
 def rightTurn(l):
-
+     return False
 def convexHull(p):
      sorted(p)
      upper = [p[0], p[1]]
      i = 2
      while i < len(coordinates):
-          while len(upper) > 2 and not rightTurn
+          upper.append(p[i])
+          while len(upper) > 2 and not rightTurn([upper[-1], upper[-2], upper[-3]]):
+               upper.remove(upper[-2])
+          i+=1
+     lower = [p[-1], p[-2]]
+     i = len(coordinates) - 2
+     while i > 0:
+          lower.append(p[i])
+          while len(lower) > 2 and not rightTurn([lower[-1], lower[-2], lower[-3]]):
+               lower.remove(lower[-2])
+          i-=1
+     lower.remove(lower[-1])
+     return upper.append(lower)
 
 
 print(convexHull(coordinates))
